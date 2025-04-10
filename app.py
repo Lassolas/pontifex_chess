fd5b48fee8f563b60db4123eda79f363efd34c78
Valentin Marcel <valentinmarcel39@gmail.com>
2025-04-10 14:15:16 +0200

    correct time

--- app.py
+++ app.py
@@ -109,13 +109,13 @@ def submit_results():
 
         # Get the current date and time
         now = datetime.now()
-        date_str = now.strftime("%y-%d")  # Format: YY-DD
-        time_str = now.strftime("%H-%M")  # Format: hh-mm
+        date_str = now.strftime("%d-%m-%y")  # Format: YY-DD
+        time_str = now.strftime("%H:%M")  # Format: hh-mm
 
         # Prepare data to be written to the new sheet
         sheet_data = [
-            [f"Date: {date_str}"],  # Date row
-            [f"Time: {time_str}"],  # Time row
+            ['Date', date_str],  # Date row
+            ['Time', time_str],  # Time row
             ['Difficulty', difficulty],  # Header for Difficulty
             ['Test Duration', test_duration],  # Header for Test Duration
             ['Board Display Time', board_display_time],  # Header for Board Display Time
